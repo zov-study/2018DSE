669981634366e5f5627617cc6eff0cc4a26abcc4
zov-study@gmx.com <zov-study@gmx.com>
2018-07-09 11:01:51 +1200

    Week 2

---
+++ WEEK2/vuedsk/src/views/MainWindow.vue
@@ -0,0 +1,127 @@
+<template>
+  <Window v-bind:title='title' width="1000" height="480" margined v-on:show="show" menu="true" v-on:close="exit">
+    <Box>
+    <Group stretchy title="Input Widgets" margined width="200" height="200">
+      <Box vertical padded>
+            <Form label="Form" padded>
+              <TextInput label="First name:" v-model="name"/>
+              <TextInput label="Phone:" v-model="phone"/>
+              <DropdownList stretchy v-bind:items="districts" v-model="item"/>
+              <TextInput label="Email:" v-model="email"/>
+              <!-- <Spinbox label="Age:" min="1" max="99"/> -->
+              <Button @click="Submit">Submit</Button>
+            </Form>
+
+        <Button @click="showModal" style="bgcolor:green;">Click me</Button>
+      </Box>
+    </Group>
+    </Box>  
+  </Window>
+</template>
+
+<script>
+  import libui from 'libui-node'
+  import Vue from 'vuido'
+  import WaitWindow from './WaitWindow'
+  import crud from '../db/crud'
+
+  export default {
+    data() {
+      return {
+        title:'Main window',
+        text: 'Text input',
+        name: 'Vasia',
+        phone:'464646464',
+        email:'asd@bsd.com',
+        districts: ['Arch Hill',
+                  'Auckland CBD',
+                  'Avondale',
+                  'Blockhouse Bay',
+                  'Balmoral',
+                  'Eden Terrace',
+                  'Eden Valley',
+                  'Ellerslie',
+                  'Epsom',
+                  'Freemans Bay',
+                  'Glendowie',
+                  'Glen Innes',
+                  'Grafton',
+                  'Greenlane',
+                  'Greenwoods Corner',
+                  'Grey Lynn',
+                  'Herne Bay',
+                  'Hillsborough',
+                  'Kingsland',
+                  'Kohimarama',
+                  'Lynfield',
+                  'Meadowbank',
+                  'Mission Bay',
+                  'Morningside',
+                  'Mount Albert',
+                  'Mount Eden',
+                  'Mount Roskill',
+                  'Mount Wellington',
+                  'Newmarket',
+                  'Newton',
+                  'New Windsor',
+                  'Onehunga',
+                  'One Tree Hill',
+                  'Orakei',
+                  'Oranga',
+                  'Owairaka',
+                  'Panmure',
+                  'Parnell',
+                  'Penrose',
+                  'Point England',
+                  'Point Chevalier',
+                  'Ponsonby',
+                  'Remuera',
+                  'Royal Oak',
+                  'Saint Heliers',
+                  'Saint Johns',
+                  'Saint Marys Bay',
+                  'Sandringham',
+                  'Stonefields',
+                  'Tamaki',
+                  'Te Papapa',
+                  'Three Kings',
+                  'Waikowhai',
+                  'Waterview',
+                  'Western Springs',
+                  'Westfield',
+                  'Westmere'],
+        item: 25
+      };
+    },
+    computed: {
+    },
+    methods: {
+      Submit(){
+        let customer = {
+            name: this.name,
+            phone: this.phone,
+            district:this.districts[this.item],
+            email: this.email,
+            date: Date.now()
+        };
+        crud.newCustomer(customer);
+      },
+      show(){
+        console.log("Main window");
+      },
+      showModal(){
+        let customer = crud.findByPhone(this.phone);
+        let ww = new Vue({
+            render: h=>h(WaitWindow)
+        });      
+        // ww.title = customer.name;
+        console.log(ww.$root._data, customer[0].name);
+        ww.$mount();
+        console.log("Click");
+      },
+      exit() {
+        libui.stopLoop();
+      }
+    }
+  }
+</script>
---
+++ WEEK2/vuedsk/src/views/WaitWindow.vue
@@ -0,0 +1,52 @@
+<template>
+  <Window v-bind:title="title" width="500" height="300" margined v-on:show="show" v-on:close="exit">
+    <Box>
+      <Group v-for='(val,ind) in arr' title="Input Widgets" margined width="200">
+        <Box horizontal padded >
+          <Text width="100"> {{val+1}}</Text>
+          <Text width="200"> {{name}}</Text>
+          <Button @click="edit" >Edit</Button>
+        </Box>
+      </Group>
+        <Box horizontal padded>
+          <Button @click="add" >Add</Button>
+          <Button @click="exit" >Exit</Button>
+        </Box>
+    </Box>  
+  </Window>
+</template>
+
+<script>
+  import wlibui from 'libui-node'
+
+  export default {
+    data() {
+      return {
+        name: 'Text input',
+        title: 'Wait Window',
+        arr: []
+      };
+    },
+    computed: {
+    },
+    methods: {
+      ToggleBorder(){
+        console.log(this.$root.$el.childNodes[0].childNodes[0].attributes.title);
+        this.$root.$el.childNodes[0].childNodes[0].attributes.title="Is it work?";
+      },
+      show(){
+        // console.log(this.$root.$el.attributes);
+      },
+      edit() {
+        console.log('Edit click');
+      },
+      add() {
+        console.log(this.arr.length);
+        this.arr.push(this.arr.length);
+      },
+      exit() {
+        this.$root.$destroy()
+      }
+    }
+  }
+</script>
